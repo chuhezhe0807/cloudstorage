4bacd625ddd79111044210d63ac59da4f4542576
chuhezhe <chuhezhe@example.com>
2026-06-28 19:13:06 +0800

    chore: 编译报错修改;

--- user-service/src/test/java/com/chuhezhe/user/controller/UserControllerTest.java
+++ user-service/src/test/java/com/chuhezhe/user/controller/UserControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
